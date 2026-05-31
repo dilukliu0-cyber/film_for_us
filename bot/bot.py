082b06c8dd6f8cece92a4d7d51c59a93725cb0c7
Diluk <diluk@example.com>
2026-05-31 20:06:01 +0200

    v1.2: Update bot version

--- bot/bot.py
+++ bot/bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-VERSION = "1.1"  # Версия бота
+VERSION = "1.2"  # Версия бота
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
